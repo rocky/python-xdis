1e8fee88ac636392a893421b387c4272258988f6
rocky <rb@dustyfeet.com>
2016-09-13 00:32:24 -0400

    Avoid timestamp in pytest

--- pytest/test_disasm.py
+++ pytest/test_disasm.py
@@ -28,8 +28,8 @@ def test_funcoutput(capfd, test_tuple, function_to_test):
     in_file, filename_expected = test_tuple
     resout = StringIO()
     function_to_test(in_file, resout)
-    expected = open(filename_expected, "r").read()
-    got = "\n".join(resout.getvalue().split("\n")[1:])
+    expected = "".join(open(filename_expected, "r").readlines()[1:])
+    got = "\n".join(resout.getvalue().split("\n")[2:])
     if got != expected:
         with open(filename_expected + ".got", "w") as out:
             out.write(got)
