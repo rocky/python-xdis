11e4a4de072d91b1309c786441652b010dd30274
rocky <rb@dustyfeet.com>
2016-10-21 07:52:13 -0400

    Add Python 2.1 bytecode

--- test/test_pythonlib.py
+++ test/test_pythonlib.py
@@ -71,7 +71,8 @@ test_options = {
                   PYOC, 'base_2.7', 2.7),
 }
 
-for vers in (1.5, 2.2, 2.3, 2.4, 2.5, '2.5dropbox', 2.6, 2.7,
+for vers in (1.5, 2.1, 2.2, 2.3, 2.4, 2.5, '2.5dropbox',
+             2.6, 2.7,
              3.2, 3.3, 3.4, 3.5, 'pypy3.2', 'pypy2.7',
              3.6):
     bytecode = "bytecode_%s" % vers
