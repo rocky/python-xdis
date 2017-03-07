22ba1e5a62a76027d33e71d129e372cf7c066490
Kirill Spitsyn <tn@0x746e.org.ua>
2017-03-07 14:41:25 -0800

    Don't assume `bytecode-` dirs are in the current directory.

--- test/test_pythonlib.py
+++ test/test_pythonlib.py
@@ -77,7 +77,7 @@ for vers in (1.5, 2.1, 2.2, 2.3, 2.4, 2.5, '2.5dropbox',
              3.6):
     bytecode = "bytecode_%s" % vers
     key = "bytecode-%s" % vers
-    test_options[key] =  (bytecode, PYC, bytecode, vers)
+    test_options[key] = (os.path.join(src_dir, bytecode), PYC, bytecode, vers)
     key = "%s" % vers
     pythonlib = "python%s" % vers
     if isinstance(vers, float) and vers >= 3.0:
