04e21c7ddfed02a011e4c9012643d4e5f536d1a6
rocky <rb@dustyfeet.com>
2017-06-28 08:28:13 -0400

    Another magic test for magic coverage

--- test_unit/test_magic.py
+++ test_unit/test_magic.py
@@ -1,4 +1,4 @@
-import unittest, imp
+import unittest, imp, sys
 from xdis import PYTHON_VERSION
 import xdis.magics as magics
 
@@ -7,6 +7,12 @@ class TestOpcodes(unittest.TestCase):
     def test_basic(self):
         """Basic test of magic numbers"""
         current = imp.get_magic()
+        if hasattr(sys, 'version_info'):
+            version = '.'.join([str(v) for v in sys.version_info[0:3]])
+            self.assertTrue(version in magics.magics.keys(),
+                            "version %s is not in magic.magics.keys: %s" %
+                            (version, magics.magics.keys()))
+
         self.assertEqual(current, magics.int2magic(magics.magic2int(current)))
         self.assertTrue(str(PYTHON_VERSION) in magics.magics.keys(),
                         "PYTHON VERSION %s is not in magic.magics.keys: %s" %
