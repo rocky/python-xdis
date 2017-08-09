f580209dcb711b33d32b0e6f95b43c15998c0970
rocky <rocky@dusytfeet.com>
2017-08-09 11:23:04 -0400

    Pypy fixes

--- xdis/magics.py
+++ xdis/magics.py
@@ -175,11 +175,13 @@ add_canonic_versions('2.6 2.6.6 2.6.7 2.6.8 2.6.9', '2.6a1')
 add_canonic_versions('2.7.1 2.7.2 2.7.2 2.7.3 2.7.4 2.7.5 2.7.6 2.7.7 '
                      '2.7.8 2.7.9 2.7.10 2.7.11 2.7.12 2.7.13', '2.7')
 add_canonic_versions('3.2 3.2.0 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 3.2.6', '3.2a2')
-add_canonic_versions('3.2.5pypy', '3.2pypy')
 add_canonic_versions('3.3 3.3.1 3.3.0 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6', '3.3a4')
 add_canonic_versions('3.4 3.4.0 3.4.1 3.4.2 3.4.3 3.4.4 3.4.5 3.4.6', '3.4rc2')
 add_canonic_versions('3.5.0 3.5.1 3.5.2', '3.5')
 add_canonic_versions('3.6 3.6.0 3.6.1 3.6.2', '3.6.0rc1')
+
+add_canonic_versions('3.2.5pypy', '3.2pypy')
+add_canonic_versions('2.7.10pypy', '2.7pypy')
 
 # The canonic version for a canonic version is itself
 for v in versions.values():
