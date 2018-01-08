51779c8803804b95258245d50643db8200ddd1f0
rocky <rocky@dusytfeet.com>
2018-01-08 16:32:50 -0500

    Add magic aliases for 3.3.7 and 3.6.4

--- xdis/magics.py
+++ xdis/magics.py
@@ -245,11 +245,11 @@ add_canonic_versions('2.7.1 2.7.2 2.7.2 2.7.3 2.7.4 2.7.5 2.7.6 2.7.7 '
 add_canonic_versions('3.0 3.0.0 3.0.1',                                '3.0a5')
 add_canonic_versions('3.1 3.1.0 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5',        '3.1a0+')
 add_canonic_versions('3.2 3.2.0 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 3.2.6',  '3.2a2')
-add_canonic_versions('3.3 3.3.1 3.3.0 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6 3.3.7rc1', '3.3a4')
+add_canonic_versions('3.3 3.3.1 3.3.0 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6 3.3.7rc1 3.3.7', '3.3a4')
 add_canonic_versions('3.4 3.4.0 3.4.1 3.4.2 3.4.3 3.4.4 3.4.5 3.4.6 3.4.7', '3.4rc2')
 add_canonic_versions('3.5.0 3.5.1 3.5.2', '3.5')
 add_canonic_versions('3.5.3 3.5.4', '3.5.3')
-add_canonic_versions('3.6 3.6.0 3.6.1 3.6.2 3.6.3', '3.6.0rc1')
+add_canonic_versions('3.6 3.6.0 3.6.1 3.6.2 3.6.3', '3.6.4', '3.6.0rc1')
 
 add_canonic_versions('2.7.10pypy 2.7.13pypy', '2.7pypy')
 add_canonic_versions('2.7.3b0Jython', '2.7.1b3Jython')
