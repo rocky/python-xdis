c5a5e35589ab406fc69adb8eadc4e7b911a1cfec
rocky <rocky@dusytfeet.com>
2018-01-22 11:25:51 -0500

    More admnistrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 PYTHON_VERSION=2.7.14
 
+if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
 git checkout master && git pull && pyenv local $PYTHON_VERSION
--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 PYTHON_VERSION=2.4.6
 
+if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
 git checkout python-2.4  && git pull && pyenv local $PYTHON_VERSION
