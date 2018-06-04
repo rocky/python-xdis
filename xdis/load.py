643b65a4b9bd30eed96c4db65a881a82ac19e7b8
rocky <rb@dustyfeet.com>
2018-06-04 08:45:20 -0400

    2.4 doesn't do "with'

--- xdis/load.py
+++ xdis/load.py
@@ -102,9 +102,12 @@ def load_module(filename, code_objects=None, fast_load=False,
     elif osp.getsize(filename) < 50:
         raise ImportError("File name: '%s (%d bytes)' is too short to be a valid pyc file" % (filename, osp.getsize(filename)))
 
-    with open(filename, 'rb') as fp:
+    try:
+        fp = open(filename, 'rb')
         return load_module_from_file_object(fp, filename=filename, code_objects=code_objects,
                                             fast_load=fast_load, get_code=get_code)
+    finally:
+        fp.close()
 
 def load_module_from_file_object(fp, filename='<unknown>', code_objects=None, fast_load=False,
                 get_code=True):
