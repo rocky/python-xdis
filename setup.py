4fdb7063f43b3828c827a1ba9b1b7d7a72d7ec30
rocky <rb@dustyfeet.com>
2019-06-21 06:31:48 -0400

    Ensure pydisasm installs
    looks like "script" no longer works; "entry_points" still does though

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Setup script for the 'xdis' distribution."""
 
 from __pkginfo__ import \
-    author,           author_email,       \
+    author,           author_email,       entry_points, \
     license,          long_description,   classifiers,               \
     modname,          py_modules,         \
     scripts,          short_desc,         tests_require,             \
@@ -14,13 +14,13 @@ setup(
        author_email       = author_email,
        classifiers        = classifiers,
        description        = short_desc,
+       entry_points       = entry_points,
        license            = license,
        long_description   = long_description,
        name               = modname,
        packages           = find_packages(),
        py_modules         = py_modules,
        # setup_requires     = setup_requires,
-       scripts            = scripts,
        tests_require      = tests_require,
        url                = web,
        version            = VERSION,
