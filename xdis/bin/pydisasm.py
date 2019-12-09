5fe1d9988d353da92d61e90101a92de4503766a2
rocky <rocky@dusytfeet.com>
2019-12-08 19:05:53 -0500

    Some tolerance for 3.9.0.alpha1
    More work is needed though.

--- xdis/bin/pydisasm.py
+++ xdis/bin/pydisasm.py
@@ -16,38 +16,46 @@ from xdis.main import disassemble_file
 
 program, ext = os.path.splitext(os.path.basename(__file__))
 
-PATTERNS = ('*.pyc', '*.pyo')
+PATTERNS = ("*.pyc", "*.pyo")
+
 
 @click.command()
-@click.option("--asm/--noasm", default=False,
-              help='Produce output suitable for the xasm assembler')
-@click.option("--show-bytes/--noshow-bytes", default=False,
-              help='include bytecode bytes in output')
+@click.option(
+    "--asm/--noasm",
+    default=False,
+    help="Produce output suitable for the xasm assembler",
+)
+@click.option(
+    "--show-bytes/--noshow-bytes",
+    default=False,
+    help="include bytecode bytes in output",
+)
 @click.version_option(version=VERSION)
-@click.option("--header/--no-header", default=False,
-              help='Show only the module header information')
-@click.argument('files', nargs=-1, type=click.Path(readable=True), required=True)
+@click.option(
+    "--header/--no-header",
+    default=False,
+    help="Show only the module header information",
+)
+@click.argument("files", nargs=-1, type=click.Path(readable=True), required=True)
 def main(asm, show_bytes, header, files):
     """Disassembles a Python bytecode file.
 
     We handle bytecode for virtually every release of Python and some releases of PyPy.
     The version of Python in the bytecode doesn't have to be the same version as
-    the Python interpreter used to run this program. For example, you can disassemble Python 3.6.1
-    bytecode from Python 2.7.13 and vice versa.
+    the Python interpreter used to run this program. For example, you can disassemble Python 3.6.9
+    bytecode from Python 2.7.15 and vice versa.
     """
-    Usage_short = """usage:
-   %s [--asm] -i FILE...
-   %s --version
-Type -h for for full help.""" % (program, program)
-
-
-    if not (2.5 <= PYTHON_VERSION <= 3.8):
-        sys.stderr(print("This works on Python version 2.5..3.8; have %s" % PYTHON_VERSION))
-
-    if not len(files):
-        sys.stderr.write("No file(s) given..\n")
-        print(Usage_short, file=sys.stderr)
-        sys.exit(1)
+    if not (2.7 <= PYTHON_VERSION <= 3.9):
+        if 2.4 <= PYTHON_VERSION <= 2.6:
+            sys.stderr.write(
+                "This code works on 2.7..3.8. code that works for %s can be found in the python-2.4 branch\n"
+                % PYTHON_VERSION
+            )
+            sys.exit(1)
+        sys.stderr.write(
+            "This works on Python version 2.7..3.8; have %s.\n" % PYTHON_VERSION
+        )
+        sys.exit(2)
 
     for path in files:
         # Some sanity checks
@@ -58,11 +66,15 @@ Type -h for for full help.""" % (program, program)
             sys.stderr.write("File name: '%s' isn't a file\n" % path)
             continue
         elif osp.getsize(path) < 50:
-            sys.stderr.write("File name: '%s (%d bytes)' is too short to be a valid pyc file\n" % (path, osp.getsize(path)))
+            sys.stderr.write(
+                "File name: '%s (%d bytes)' is too short to be a valid pyc file\n"
+                % (path, osp.getsize(path))
+            )
             continue
 
         disassemble_file(path, sys.stdout, asm, header, show_bytes)
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv[1:])
