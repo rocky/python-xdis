5acb07f004af1e0c3d2062cdd3c945deed90e24a
rocky <rocky@dusytfeet.com>
2021-02-27 08:37:40 -0500

    Python 2.6 compatability

--- xdis/op_imports.py
+++ xdis/op_imports.py
@@ -214,7 +214,10 @@ def remap_opcodes(op_obj, alternate_opmap):
             item = list(item)
             new_frozensets[i] = copy.deepcopy(item)
 
-    opcodes_with_args = {opname: opcode for opname, opcode in op_obj.opmap.items() if opcode >= op_obj.HAVE_ARGUMENT}
+    opcodes_with_args = {}
+    for opname, opcode in op_obj.opmap.items():
+        if opcode >= op_obj.HAVE_ARGUMENT:
+            opcode_with_args[opname] = opcode
 
     for opname, alt_opcode in alternate_opmap.items():
         if opname not in op_obj.opmap:
@@ -248,7 +251,9 @@ def remap_opcodes(op_obj, alternate_opmap):
                     idx = list(getattr(op_obj, set_name)).index(original_opcode)
                     frozen_set_list[idx] = alt_opcode
 
-    new_opcodes_with_args = {opname: new_opmap[opname] for opname in opcodes_with_args.keys()}
+    new_opcodes_with_args = {}
+    for opname in opcodes_with_args.keys():
+        new_opcodes_with_args[opname] = new_opmap[opname]
     lowest_opcode_with_arg = min(new_opcodes_with_args.values())
     setattr(op_obj, 'HAVE_ARGUMENT', lowest_opcode_with_arg)
     if hasattr(op_obj, 'PJIF'):
