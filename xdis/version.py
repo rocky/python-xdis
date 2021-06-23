ae006a036c774bd5f2833837680c7b6e888d7455
rocky <rocky@dusytfeet.com>
2021-06-23 11:41:53 -0400

    black shouldn't format version.py

--- xdis/version.py
+++ xdis/version.py
@@ -3,4 +3,5 @@
 # This file is suitable for sourcing inside POSIX shell as
 # well as importing into Python. That's why there is no
 # space around "=" below.
+# fmt: off
 __version__="5.0.11.dev0"  # noqa
