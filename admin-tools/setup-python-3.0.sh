b4c862ffb7a885cde9989dfc90377ba04b6c114a
rocky <rocky@dusytfeet.com>
2021-09-11 19:36:43 -0400

    Go over pyenv versions
    Start to split out python 3.0-3.2

---
+++ admin-tools/setup-python-3.0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PYTHON_VERSION=3.0.1
+
+if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+git checkout python-3.0-to-3.2  && git pull && pyenv local $PYTHON_VERSION
