1d13878e066a7ae9246a30aa498ec74b9e910845
rocky <rocky@dusytfeet.com>
2021-09-11 19:50:58 -0400

    Version splitting

--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PYTHON_VERSION=3.0.1
+pyenv local $PYTHON_VERSION
 
 if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
