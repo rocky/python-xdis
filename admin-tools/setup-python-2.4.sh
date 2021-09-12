3acdb6003c2aa35b3dd2d114c2da174a253009ab
rocky <rocky@dusytfeet.com>
2021-09-11 20:01:51 -0400

    Start Python 3.1 to 3.2 branch

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -6,4 +6,4 @@ if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
     exit 1
 fi
 
-git checkout python-2.4-to-3.2  && git pull && pyenv local $PYTHON_VERSION
+git checkout python-2.4-to-2.7  && git pull && pyenv local $PYTHON_VERSION
