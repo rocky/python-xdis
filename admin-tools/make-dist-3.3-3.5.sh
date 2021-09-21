e09a8339716cbee8c0e21fcc21dd0e2b03fb8a06
rocky <rocky@dusytfeet.com>
2021-09-21 08:43:15 -0400

    Fix wheel packaging

--- admin-tools/make-dist-3.3-3.5.sh
+++ admin-tools/make-dist-3.3-3.5.sh
@@ -33,7 +33,7 @@ for pyversion in $PYVERSIONS; do
     first_two=$(echo $pyversion | cut -d'.' -f 1-2 | sed -e 's/\.//')
     rm -fr build
     python setup.py bdist_egg bdist_wheel
-    mv -v dist/${PACKAGE}-$VERSION-{py2.py3,py$first_two}-none-any.whl
+    mv -v dist/${PACKAGE}-$VERSION-{py3,py$first_two}-none-any.whl
     echo === $pyversion ===
 done
 
