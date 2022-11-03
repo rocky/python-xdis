cd1f805b6e1b93692cee1bfce8c4ee5bd8b864a2
rocky <rocky@dusytfeet.com>
2022-11-03 15:58:33 -0400

    Better 3.0 tolerance

--- pytest/test_instructions.py
+++ pytest/test_instructions.py
@@ -119,7 +119,7 @@ pytest.mark.skipif(
 
 
 def test_inst_jumps():
-    if sys.version_info >= (2, 7):
+    if sys.version_info >= (3, 1):
         variant = "pypy" if IS_PYPY else None
         opc = get_opcode_module(sys.version_info, variant)
         bytecode_obj = Bytecode(extended_arg_fn36, opc)
