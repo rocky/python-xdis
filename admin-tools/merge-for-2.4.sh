adda14811d565b9175e56a45443cebbaff3c6ea2
rocky <rocky@dusytfeet.com>
2024-02-12 20:06:20 -0500

    Administrivia:
    merge should leave us back in the directory we started

--- admin-tools/merge-for-2.4.sh
+++ admin-tools/merge-for-2.4.sh
@@ -1,5 +1,14 @@
 #/bin/bash
+# Setup for running Python 2.4 .. 2.7, merging python-3.0-to-3.2 into this branch
+set -e
+function finish {
+  cd $owd
+}
+owd=$(pwd)
+trap finish EXIT
+
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-2.4.sh; then
     git merge python-3.0-to-3.2
 fi
+finish
