fd64c6d13b815a6f15bd85462e550790962bea1d
rocky <rocky@dusytfeet.com>
2024-03-15 11:35:46 -0400

    Get ready for release 6.1.0

--- admin-tools/make-dist-3.3-3.5.sh
+++ admin-tools/make-dist-3.3-3.5.sh
@@ -19,22 +19,40 @@ fi
 
 cd ..
 source $PACKAGE/version.py
-echo $VERSION
+echo $__version__
 
 for pyversion in $PYVERSIONS; do
     echo --- $pyversion ---
+    if [[ ${pyversion:0:4} == "pypy" ]] ; then
+	echo "$pyversion - PyPy does not get special packaging"
+	continue
+    fi
     if ! pyenv local $pyversion ; then
-	exit $?
+  	    exit $?
     fi
     # pip bdist_egg create too-general wheels. So
     # we narrow that by moving the generated wheel.
 
-    # Pick out first two number of version, e.g. 3.5.1 -> 35
+    # Pick out first two numbers of version, e.g. 3.5.1 -> 35
     first_two=$(echo $pyversion | cut -d'.' -f 1-2 | sed -e 's/\.//')
     rm -fr build
     python setup.py bdist_egg bdist_wheel
-    mv -v dist/${PACKAGE}-$VERSION-{py3,py$first_two}-none-any.whl
+    if [[ $first_two =~ py* ]]; then
+	if [[ $first_two =~ pypy* ]]; then
+	    # For PyPy, remove the what is after the dash, e.g. pypy37-none-any.whl instead of pypy37-7-none-any.whl
+	    first_two=${first_two%-*}
+	fi
+	mv -v dist/${PACKAGE}-$__version__-{py3,$first_two}-none-any.whl
+    else
+	mv -v dist/${PACKAGE}-$__version__-{py3,py$first_two}-none-any.whl
+    fi
     echo === $pyversion ===
 done
 
 python ./setup.py sdist
+
+tarball=dist/${PACKAGE}-${__version__}.tar.gz
+if [[ -f $tarball ]]; then
+    mv -v $tarball dist/${PACKAGE}_33-${__version__}.tar.gz
+fi
+finish
