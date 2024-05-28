9aaa9b1aecdded63a2326769f03047cd17e79c5b
rocky <rocky@dusytfeet.com>
2024-05-28 07:42:02 -0400

    Use 3.12 in master branch now
    The former 3.10 is for the 3.6-3.10 branch
    
    Fix up merge from 3.6 so that it sets Python 3.6

--- admin-tools/setup-python-3.6.sh
+++ admin-tools/setup-python-3.6.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PYTHON_VERSION=3.6.15
-pyenv local $PYTHON_VERSION
-
 if [[ $0 == $${BASH_SOURCE[0]} ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
+
+PYTHON_VERSION=3.6
+pyenv local $PYTHON_VERSION
 
 git checkout python-3.6-to-3.10 && git pull && pyenv local $PYTHON_VERSION
