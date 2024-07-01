792b2118715340f818e0f90d8e8c48f97db562b3
rocky <rocky@dusytfeet.com>
2024-07-01 05:53:46 -0400

    In 2.x dis.disassemble doesn't support out=

--- xdis/disasm.py
+++ xdis/disasm.py
@@ -225,11 +225,11 @@ def disco_loop(
         if asm_format == "dis":
             assert (
                 version_tuple[:2] == PYTHON_VERSION_TRIPLE[:2]
-            ), "dis requires disassembly from the same Python verison: "(
+            ), ("dis requires disassembly from the same Python version: "
                 "Bytecode is for %s; Running: %s"
                 % (version_tuple[:2], PYTHON_VERSION_TRIPLE[:2])
             )
-            dis.disassemble(co, lasti=-1, file=real_out)
+            dis.disassemble(co, lasti=-1)
         else:
             bytecode = Bytecode(co, opc, dup_lines=dup_lines)
             real_out.write(
