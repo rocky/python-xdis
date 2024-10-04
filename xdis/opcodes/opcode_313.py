c2c44506dd37960eab94d5f1e9fa189282e3e24c
elliot <elliot@utdallas.edu>
2024-10-03 19:13:41 -0500

    init 3.13 opcodes (unfinished)

--- xdis/opcodes/opcode_313.py
+++ xdis/opcodes/opcode_313.py
@@ -25,10 +25,27 @@ init_opdata(loc, opcode_312, version_tuple)
 ## These are removed / replaced since 3.12...
 #           OP NAME                      OPCODE
 #----------------------------------------------
+rm_op(loc, "LOAD_CLOSURE", 136)  # now a psuedo-instruction, replaced with LOAD_FAST
 
 ## These are new since 3.12...
 #            OP NAME                              OPCODE POP PUSH
 # ---------------------------------------------------------------
+def_op(loc, "TO_BOOL", 40, 0, 0)
+def_op(loc, "CALL_KW", 57, 2, 1)  # will pop more depending on arg, pushes ret val
+def_op(loc, "SET_FUNCTION_ATTRIBUTE",106, 2, 1)  # pops func and attr val then pushes func back on stack
+def_op(loc, "CONVERT_VALUE", 1, 1)
+def_op(loc, "FORMAT_SIMPLE", 1, 1)
+def_op(loc, "FORMAT_SPEC", 2, 1)
+
+def_op(loc, "LOAD_CLOSURE", 258)  # psuedo-instruction
+
+
+### update opinfo tables
+# add new table "hasjump"
+loc.update({"hasjump": [72, 77, 78, 79, 97, 98, 99, 100, 104, 256, 257]})
+loc["hasjrel"] = loc["hasjump"]
+
+# TODO continue updating tables
 
 ### update formatting
 opcode_arg_fmt313 = opcode_312.opcode_arg_fmt12
