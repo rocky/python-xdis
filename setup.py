9f9c1fd3db5cc775a3aaf4c99b71cd46a1793bf3
rocky <rocky@dusytfeet.com>
2025-10-12 09:56:17 -0400

    Merge woes

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@
   Note: that we use 2.x compatible Python code here.
 """
 import sys
-from xdis.version import __version__
 from setuptools import find_packages, setup
 
 from __pkginfo__ import (
@@ -23,22 +22,25 @@ from __pkginfo__ import (
     web,
     zip_safe,
 )
+from xdis.version import __version__
 
-if major != 3 or not 3 <= minor < 6:
-    sys.stderr.write("This installation medium is only for Python 3.3 to 3.5. You are running Python %s.%s.\n" % (major, minor))
+major = sys.version_info[0]
+minor = sys.version_info[1]
+
+if major != 3 or not 0 <= minor < 3:
+    sys.stderr.write("This installation medium is only for Python 3.0 to 3.2. You are running Python %s.%s.\n" % (major, minor))
 elif major == 3 and minor >= 11:
     sys.stderr.write("Please install using xdis-x.y.z.tar.gz from https://github.com/rocky/python-xdis/releases\n")
     sys.exit(1)
 elif major == 6 and minor < 11:
     sys.stderr.write("Please install using xdis_36-x.y.z.tar.gz from https://github.com/rocky/python-xdis/releases\n")
     sys.exit(1)
-if major == 3 and 0 <= minor <= 2:
-    sys.stderr.write("Please install using xdis_30-x.y.z.tar.gz from https://github.com/rocky/python-xdis/releases\n")
+if major == 3 and 3 <= minor < 6:
+    sys.stderr.write("Please install using xdis_33-x.y.z.tar.gz from https://github.com/rocky/python-xdis/releases\n")
     sys.exit(1)
 elif major == 2:
     sys.stderr.write("Please install using xdis_24-x.y.z.tar.gz from https://github.com/rocky/python-xdis/releases\n")
     sys.exit(1)
->>>>>>> python-3.3-to-3.5
 
 
 setup(
