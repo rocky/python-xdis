1cab5639f2a47670e3a40fb27ec9190626fb6864
rocky <rocky@dusytfeet.com>
2025-10-23 06:46:37 -0400

    Add is_fixed_worksize_bytecode()
    local Variable is_python36 changed to is_fixed_wordsize_bytecode() function. This
    clarifies *why* this distinction is important. This function can be used
    outside of xdis (and is).
    
    Also, add more annotations.

--- xdis/magics.py
+++ xdis/magics.py
@@ -36,7 +36,7 @@ import re
 import struct
 import sys
 from importlib.util import MAGIC_NUMBER as MAGIC
-from typing import Dict, Set
+from typing import Dict, Set, Tuple
 
 from xdis.version_info import IS_GRAAL, IS_PYPY, IS_RUST, version_tuple_to_str
 
@@ -799,7 +799,7 @@ def magic_int2tuple(magic_int: int) -> tuple:
     return py_str2tuple(magicint2version[magic_int])
 
 
-def py_str2tuple(orig_version: str) -> tuple[int, int] | tuple[int, int, int]:
+def py_str2tuple(orig_version: str) -> Tuple[int, int] | Tuple[int, int, int]:
     """Convert a Python version into a tuple number,
     e.g. (2, 5), (3, 6).
 
