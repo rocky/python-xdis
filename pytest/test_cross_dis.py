4fb9e4d4881919cc21ae601d5036bb6a3bdd5709
rocky <rocky@dusytfeet.com>
2025-11-09 12:19:17 -0500

    Merge stuff

--- pytest/test_cross_dis.py
+++ pytest/test_cross_dis.py
@@ -2,14 +2,13 @@ from dis import findlabels as findlabels_std
 
 from xdis.cross_dis import findlabels
 from xdis.op_imports import get_opcode_module
-from xdis.version_info import IS_GRAAL, PYTHON_IMPLEMENTATION, PYTHON_VERSION_TRIPLE
+from xdis.version_info import PYTHON_IMPLEMENTATION, PYTHON_VERSION_TRIPLE
 
 
 def test_findlabels():
     code = findlabels.__code__.co_code
     opc = get_opcode_module(PYTHON_VERSION_TRIPLE, PYTHON_IMPLEMENTATION)
 
->>>>>>> python-3.3-to-3.5
     assert findlabels(code, opc) == findlabels_std(code)
 
 
